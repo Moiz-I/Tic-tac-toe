2d760cfd5f2cc6c5a5cf745e921a9a983e81a8d9
Moiz _ <sky.moizi9@gmail.com>
2021-04-01 15:47:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+theBoard =    {'7': ' ' , '8': ' ' , '9': ' ' ,
+            '4': ' ' , '5': ' ' , '6': ' ' ,
+            '1': ' ' , '2': ' ' , '3': ' ' }
+            
+def print_board(board):
+    print(board['7'] + '|' + board['8'] + '|' + board['9'])
+    print('-+-+-')
+    print(board['4'] + '|' + board['5'] + '|' + board['6'])
+    print('-+-+-')
+    print(board['1'] + '|' + board['2'] + '|' + board['3'])
+
+def game(print_board,theBoard):
+  
+
+
+#def available_moves()
+
+#def has_won()
+
+print_board(theBoard)
