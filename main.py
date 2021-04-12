5fce1478462254a97d5849f697de252e30228c34
Moiz _ <>
2021-04-12 09:13:14 +0000

    new board look

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
-theBoard =    {'7': ' ' , '8': ' ' , '9': ' ' ,
-            '4': ' ' , '5': ' ' , '6': ' ' ,
-            '1': ' ' , '2': ' ' , '3': ' ' }
+my_board = [
+	["1", "2", "X"],
+	["4", "5", "6"],
+	["7", "8", "9"]
+]
+
             
 def print_board(board):
-    print(board['7'] + '|' + board['8'] + '|' + board['9'])
-    print('-+-+-')
-    print(board['4'] + '|' + board['5'] + '|' + board['6'])
-    print('-+-+-')
-    print(board['1'] + '|' + board['2'] + '|' + board['3'])
+    print("|-------------|")
+    print("| Tic Tac Toe |")
+    print("|-------------|")
+    print("|             |")
+    print("|    " + board[0][0] + " " + board[0][1] + " " + board[0][2] + "    |")
+    print("|    " + board[1][0] + " " + board[1][1] + " " + board[1][2] + "    |")
+    print("|    " + board[2][0] + " " + board[2][1] + " " + board[2][2] + "    |")
+    print("|             |")
+    print("|-------------|")
+    print()
 
-def game(print_board,theBoard):
-  move = input("where to put: ")
-  theBoard[move] = "X"
-  print_board(theBoard)
+print_board(my_board)
+
+def game(print_board,board):
+  game_finished = False
+  while game_finished == False:
+    x_move = input("where to put X: ")
+    theBoard[x_move] = "X"
+    print_board(theBoard)
+
+    o_move = input("where to put O: ")
+    theBoard[o_move] = "O"
+    print_board(theBoard)
+    
 
 
 #def available_moves()
 
 #def has_won()
 
-game(print_board,theBoard)
+#game(print_board,theBoard)
