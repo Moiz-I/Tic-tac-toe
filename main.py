e5265919e7a2c727925a1a86159b8f11e4b6c786
Moiz _ <>
2021-04-23 09:31:32 +0000

    working tictactoe 1 loop game

--- main.py
+++ main.py
@@ -4,36 +4,84 @@ my_board = [
 	["7", "8", "9"]
 ]
 
-            
-def print_board(board):
+
+class Tictactoe():
+  def __init__(self, board):
+    self.board = board
+  
+  def print_board(self):
     print("|-------------|")
     print("| Tic Tac Toe |")
     print("|-------------|")
     print("|             |")
-    print("|    " + board[0][0] + " " + board[0][1] + " " + board[0][2] + "    |")
-    print("|    " + board[1][0] + " " + board[1][1] + " " + board[1][2] + "    |")
-    print("|    " + board[2][0] + " " + board[2][1] + " " + board[2][2] + "    |")
+    print("|    " + self.board[0][0] + " " + self.board[0][1] + " " + self.board[0][2] + "    |")
+    print("|    " + self.board[1][0] + " " + self.board[1][1] + " " + self.board[1][2] + "    |")
+    print("|    " + self.board[2][0] + " " + self.board[2][1] + " " + self.board[2][2] + "    |")
     print("|             |")
     print("|-------------|")
     print()
 
-print_board(my_board)
+  def available_moves(self):
+    moves = []
+    for row in self.board:
+      for col in row:
+        if col != "X" and col != "O":
+          moves.append(int(col))
+    return moves
 
-def game(print_board,board):
-  game_finished = False
-  while game_finished == False:
-    x_move = input("where to put X: ")
-    theBoard[x_move] = "X"
-    print_board(theBoard)
+  def select_space(self, move, turn):
+    row = (move-1)//3
+    col = (move-1)%3
+    self.board[row][col] = "{}".format(turn)
 
-    o_move = input("where to put O: ")
-    theBoard[o_move] = "O"
-    print_board(theBoard)
-    
+  def has_won(self, player):
+    for row in self.board:
+        if row.count(player) == 3:
+            return True
+    for i in range(3):
+        if self.board[0][i] == player and self.board[1][i] == player and self.board[2][i] == player:
+            return True
+    if self.board[0][0] == player and self.board[1][1] == player and self.board[2][2] == player:
+        return True
+    if self.board[0][2] == player and self.board[1][1] == player and board[2][0] == player:
+        return True
+    return False
+  
+  def game_over(self):
+    self.available_moves()
+    if self.has_won("X") or self.has_won("O") or len(self.available_moves()) ==0:
+      return True
+    else:
+      return False
+
+  def get_player_move(self, turn):
+    move = int(input("Player {player} make your turn: ".format(player=turn)))
+    self.available_moves()
+    while move not in range (1,10) or move not in self.available_moves():
+      move = int(input("Invalid move, try again: "))
+    return move
+      
+
+     
+def game(board):
+  tictactoe = Tictactoe(my_board)
+  while not tictactoe.game_over():
+    tictactoe.print_board()
+    x_move = tictactoe.get_player_move("X")
+    tictactoe.select_space(x_move,"X")
+    tictactoe.print_board()
+    if tictactoe.game_over():
+      break
+
+    o_move = tictactoe.get_player_move("O")
+    tictactoe.select_space(o_move,"O")
+  if tictactoe.has_won("X"):
+    print("X wins")
+  elif tictactoe.has_won("O"):
+    print("O wins")
+  else:
+    print("tie")
 
 
-#def available_moves()
 
-#def has_won()
-
-#game(print_board,theBoard)
+game(my_board)
